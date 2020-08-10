4a0ee680bf49810ac06c970b448c0c8aa621c8eb
Jonas Ryser <jory3@users.noreply.github.com>
2020-08-10 17:35:26 +0200

    Change fuzzy search mode
    when looking for "world" now "world2" is included as well

--- src/TNTSearch.php
+++ src/TNTSearch.php
@@ -13,6 +13,8 @@ use yii\base\Configurable;
 
 use TeamTNT\TNTSearch\TNTSearch as BaseTNTSearch;
 
+use PDO;
+
 /**
  * Class TNTSearch base on [[\TeamTNT\TNTSearch\TNTSearch]] implementing yii configurable.
  *
@@ -31,6 +33,37 @@ class TNTSearch extends BaseTNTSearch implements Configurable
         }
 
         parent::__construct();
+    }
+
+    /**
+     * @param      $keyword
+     * @param bool $isLastWord
+     *
+     * @return array
+     */
+    public function getWordlistByKeyword($keyword, $isLastWord = false)
+    {
+        if ($this->fuzziness) {
+            return $this->fuzzySearch($keyword);
+        }
+
+        $searchWordlist = "SELECT * FROM wordlist WHERE term like :keyword LIMIT 1";
+        $stmtWord       = $this->index->prepare($searchWordlist);
+
+        if ($this->asYouType && $isLastWord) {
+            $searchWordlist = "SELECT * FROM wordlist WHERE term like :keyword ORDER BY length(term) ASC, num_hits DESC LIMIT 1";
+            $stmtWord       = $this->index->prepare($searchWordlist);
+            $stmtWord->bindValue(':keyword', mb_strtolower($keyword)."%");
+        } else {
+            $stmtWord->bindValue(':keyword', mb_strtolower($keyword));
+        }
+        $stmtWord->execute();
+        $res = $stmtWord->fetchAll(PDO::FETCH_ASSOC);
+
+        if ($this->fuzziness && !isset($res[0])) {
+            return $this->fuzzySearch($keyword);
+        }
+        return $res;
     }
 
 }
